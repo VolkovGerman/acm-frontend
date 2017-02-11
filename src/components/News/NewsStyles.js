fbbfc2e7bd5df26dc7a2bddc879a62d4d505ceb7
Igor-Losev <pixxxelolo@gmail.com>
2017-02-12 02:40:45 +0300

    S1T14 Date format package, news block styles

---
+++ src/components/News/NewsStyles.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'aphrodite/no-important';
+
+export default StyleSheet.create({
+    newsItem: {
+        borderBottom: '1px solid #000',
+        paddingBottom: '20px'
+    },
+    newsItem__date: {
+        float: 'right',
+        margin: '5px 0 5px 20px'
+    }
+});
