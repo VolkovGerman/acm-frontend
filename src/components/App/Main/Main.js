559bd3f4f3b94e264e2ef5632d830ab2629f1b9a
Igor-Losev <pixxxelolo@gmail.com>
2017-02-16 03:10:45 +0300

    About block html+styles

--- src/components/App/Main/Main.js
+++ src/components/App/Main/Main.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 
 import FirstScreen from './FirstScreen/FirstScreen';
 import News from './News/News';
+import About from './About/About';
 
 class Main extends Component {
     render() {
         return (
             <div>
                 <FirstScreen onLoad={this.props.onLoad} />
+                <About />
                 <News />
             </div>
         );
