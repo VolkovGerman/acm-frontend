ebf84c6521d238dc6af93b4502a582076c99964b
nikitapavets <nikitapavets@gmail.com>
2017-02-18 19:45:59 +0300

    init new project structure

--- src/components/AboutComponent/AboutStyles.js
+++
@@ -1,7 +0,0 @@
-import { StyleSheet } from 'aphrodite/no-important';
-
-export default StyleSheet.create({
-    about: {
-        background: '#ccfccf'
-    }
-});
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-
-import menu from './menu';
-
-export default combineReducers({
-    routing: routerReducer,
-    menu
-});
