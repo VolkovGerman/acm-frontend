32d4e709b29f9b4dbf08e9789ee10ea2a802ca04
German Volkov <Volkov.german.1997@gmail.com>
2017-02-24 16:33:34 +0300

    champ item page

--- src/client/routes.jsx
+++ src/client/routes.jsx
@@ -4,16 +4,18 @@ import {Route, IndexRoute} from 'react-router';
 import Client from './components/Client';
 import HomePage from './components/HomePage/Home';
 import ChampsPage from './components/ChampsPage/Champs';
+import ChampsItemPage from './components/ChampItemPage/ChampItem';
 import NewsPage from './components/NewsPage/News';
 import NotFoundPage from './components/NotFound404Page/NotFound404';
 
 export const routes = (
     <div>
         <Route path='/' component={Client}>
-            <IndexRoute component={HomePage}/>
-            <Route path="/champs" component={ChampsPage}/>
-            <Route path="/news/:news_id" component={NewsPage}/>
+            <IndexRoute component={HomePage} />
+            <Route path="/champs" component={ChampsPage} />
+            <Route path="/champs/:id" component={ChampsItemPage} />
+            <Route path="/news/:news_id" component={NewsPage} />
         </Route>
-        <Route path="*" component={NotFoundPage}/>
+        <Route path="*" component={NotFoundPage} />
     </div>
 )
