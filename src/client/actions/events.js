b5e0f67cf50baf33dd54993898edfd124bd227a4
nikitapavets <nikitapavets@gmail.com>
2017-04-11 23:13:54 +0300

    add events action

---
+++ src/client/actions/events.js
@@ -0,0 +1,40 @@
+import config from '../../core/config/general.config';
+import fetch from 'isomorphic-fetch';
+
+import { FETCH_NEWS_REQUEST } from '../actions-types/news';
+
+function fetchNewsRequest() {
+    return {
+        type: FETCH_NEWS_REQUEST
+    }
+}
+
+import { FETCH_NEWS_SUCCESS } from '../actions-types/news';
+
+function fetchNewsSuccess(payload) {
+    return {
+        type: FETCH_NEWS_SUCCESS,
+        payload
+    }
+}
+
+import { FETCH_NEWS_FAILURE } from '../actions-types/news';
+
+function fetchNewsFailure(payload) {
+    return {
+        type: FETCH_NEWS_FAILURE,
+        payload
+    }
+}
+
+export function handleLoadingNews() {
+    return function (dispatch) {
+        dispatch(fetchNewsRequest());
+
+        return fetch(`${config.server}/api/news`)
+            .then(response => response.json())
+            .then(json => dispatch(fetchNewsSuccess(json)))
+            .catch(err => dispatch(fetchNewsFailure(err)));
+    }
+}
+
