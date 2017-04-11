e0fb5a85af5d4728185a65dc1711d7dd2287787c
VolkovGerman <Volkov.german.1997@gmail.com>
2017-04-11 23:12:33 +0300

    cors implemented

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(cors());
 
 const options = { index: false };
 app.use(express.static(path.join(__dirname, '../public/admin'), options));
