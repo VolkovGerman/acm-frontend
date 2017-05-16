1887632d44247fe7bf5186a110f15cc091a55438
nikitapavets <nikitapavets@gmail.com>
2017-05-16 11:24:25 +0300

    add  font-awesome

--- src/core/config/general.config.js
+++ src/core/config/general.config.js
@@ -1,4 +1,4 @@
 export default {
-    server: process.env.PORT || 'http://localhost:3001',
+    server: process.env.PORT || 'http://acm-frontend.herokuapp.com',
     java_server: 'http://acm-backend.herokuapp.com'
 }
